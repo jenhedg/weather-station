f937e0de6680361f3b1838cd13052ceb6eb322c8
Your Name Here <jennifernhf@gmail.com>
2020-05-24 11:20:08 -0700

    make location in api req variable, wip and a to-do for async added

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from "react";
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Enter a location");
-  const [weather, setWeather] = useState([]);
-
+  const [location, setLocation] = useState("");
+  // const [weather, getWeather] = useState(false);
+  //*****/to add an asyc await so that url loads when a city is entered but the request doesnt go out until the button is clicked currently it's hammering the api whenever a string is entered into the seach field
   useEffect(() => {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=tucson&appid=e0ae73daa44269e68dad05b97669bfe3"
-    )
-      .then((r) => r.json())
-      .then((data) => console.log(data));
-  }, [setWeather]);
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=e0ae73daa44269e68dad05b97669bfe3`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => console.log("response:", response, response.message))
+      .catch((error) => {
+        console.log(error);
+        throw error;
+      });
+  }, [location]);
 
   return (
     <div className="search-params">
@@ -20,12 +24,13 @@ const SearchParams = () => {
           <input
             id="location"
             value={location}
+            placeholder="Enter a city"
             onFocus={() => setLocation("")}
             onChange={(event) => setLocation(event.target.value)}
           ></input>
         </label>
       </form>
-      <button>Submit</button>
+      <button onClick="getWeather">Submit</button>
     </div>
   );
 };
