f198199dbdeca3082da14546004e3e99ce509595
Your Name Here <jennifernhf@gmail.com>
2020-05-25 13:45:57 -0700

    merging in async api req functionalitye

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,20 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-const SearchParams = () => {
-  const [location, setLocation] = useState("");
-  // const [weather, getWeather] = useState(false);
-  //*****/to add an asyc await so that url loads when a city is entered but the request doesnt go out until the button is clicked currently it's hammering the api whenever a string is entered into the seach field
-  useEffect(() => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=e0ae73daa44269e68dad05b97669bfe3`;
+const SearchParams = (props) => {
+  const [city, setCity] = useState("");
 
-    fetch(url)
+  const fetchData = async () => {
+    await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e0ae73dsdsfsfsdaa44269e68dad05b97669bfe3`
+    )
       .then((response) => response.json())
-      .then((response) => console.log("response:", response, response.message))
+      .then((response) => console.log("response:", response))
       .catch((error) => {
-        console.log(error);
+        console.log(error, error.message);
         throw error;
       });
-  }, [location]);
+  };
 
   return (
     <div className="search-params">
@@ -23,14 +22,13 @@ const SearchParams = () => {
           Location
           <input
             id="location"
-            value={location}
+            value={city}
             placeholder="Enter a city"
-            onFocus={() => setLocation("")}
-            onChange={(event) => setLocation(event.target.value)}
+            onChange={(event) => setCity(event.target.value)}
           ></input>
         </label>
       </form>
-      <button onClick="getWeather">Submit</button>
+      <button onClick={fetchData}>Submit</button>
     </div>
   );
 };
