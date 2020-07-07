abd53c6b172ee3908eb4e702a25dfbbac593ed85
Your Name Here <jennifernhf@gmail.com>
2020-07-07 11:39:00 -0700

    wip, mobile layout and styles

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -14,21 +14,26 @@ const Results = ({ weather }) => {
               <p>{new Date().toLocaleDateString()}</p>
             </div>
           </div>
-          <h3 className="results-temp">{Math.round(weather.main.temp)}&deg;</h3>
+          <div className="results-feature">
+            <h3 className="results-temp">
+              {Math.round(weather.main.temp)}&deg;
+            </h3>
+          </div>
         </div>
-        <div className="results-wrap-bottom">
-          <div className="icon-wrap">
+        <div className="results-bottom">
+          <div className="results-icon">
             <img
               src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png
             `}
               alt="weather icon"
             />
           </div>
-          <h3>
-            Current Conditions:
-            {` ${weather.weather[0].main} - ${weather.weather[0].description}`}
-          </h3>
-          <h3>It feels like {Math.round(weather.main.feels_like)} degrees</h3>
+          <div className="results-cond">
+            <h3>
+              {` ${weather.weather[0].main} - ${weather.weather[0].description}`}
+            </h3>
+            <h3>It feels like {Math.round(weather.main.feels_like)} degrees</h3>
+          </div>
         </div>
       </div>
     );
