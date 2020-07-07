f50166919216bd08f69ac05cc445612bbde2dc2c
Your Name Here <jennifernhf@gmail.com>
2020-07-07 12:55:35 -0700

    adjusting viewport height

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,6 +11,11 @@ function App() {
       <main className="content">
         <SearchParams />
       </main>
+      <footer>
+        <span>
+          &#169;2020 Jennifer Hedgcock | Made with &#10084; and React.js
+        </span>
+      </footer>
     </div>
   );
 }
